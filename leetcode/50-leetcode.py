8728bd9ef1d9c51b6735c418ef379ceb73b35f04
Athravsingh66 <arpit002007@gmail.com>
2026-09-13 11:38:20 +0530

    Add Leetcode Solution

---
+++ leetcode/50-leetcode.py
@@ -0,0 +1,10 @@
+# Question
+
+# Leetcode 50 - Pow(x,n)
+
+# Solution
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return pow(x,n)
+   
---
+++ leetcode/704-leetcode.py
@@ -0,0 +1,22 @@
+# Question
+
+# Leetcode 704 - Binary Search
+
+# Solution
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+                break
+            elif nums[mid]<target:
+                low=mid+1
+            elif nums[mid]>target:
+                high=mid-1        
+        return -1
+    
