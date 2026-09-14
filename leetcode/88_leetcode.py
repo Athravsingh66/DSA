1b7a572ff264be52a4bc1b1d61465f47570472f1
Athravsingh66 <arpit002007@gmail.com>
2026-09-14 19:39:50 +0530

    Add Leetcode Solution

---
+++ leetcode/88_leetcode.py
@@ -0,0 +1,11 @@
+# Question
+
+# Leetcode 88 - Merge Sorted Array
+
+# Solution
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+       
+        nums1[:m+n]=sorted(nums1[:m]+nums2[:n])
+        
