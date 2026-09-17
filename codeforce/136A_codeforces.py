0e5e31b309552b59246ae07407467fcfdf4d2231
Athravsingh66 <arpit002007@gmail.com>
2026-09-17 22:40:22 +0530

    Add Codeforces Solution

---
+++ codeforce/136A_codeforces.py
@@ -0,0 +1,15 @@
+# Question
+
+# Codeforces 136A - Presents
+
+
+# Solution
+
+n = int(input())
+p = list(map(int,input().split()))
+ans = [0] * n
+
+for i in range(n):
+    ans[p[i]-1] = i + 1
+    
+print(*ans)
